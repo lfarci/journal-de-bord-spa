022fcfce8b97d4a442cf11eb164898400cbd707e
Logan Farci <logan.farci@outlook.be>
2020-10-07 15:52:32 +0200

    The log in callback component layout is centered and show a circular progressbar

--- src/components/navigation/LogInCallback.tsx
+++ src/components/navigation/LogInCallback.tsx
@@ -1,5 +1,8 @@
+import { CircularProgress, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { AuthService } from "../../services/AuthService";
+
+import "./LogInCallback.scss";
 
 interface ILogInCallbackState {
     isLoading: boolean;
@@ -15,7 +18,7 @@ function LogInCallback() {
 
     useEffect(() => {
         new AuthService()._userManager.signinRedirectCallback().then(function () {
-            setState({isLoading: false, error: null});
+            setState({isLoading: true, error: null});
             window.location.href = "/home";
         }).catch(function (e) {
             setState({isLoading: false, error: e});
@@ -23,10 +26,12 @@ function LogInCallback() {
         });
     }, []);
 
-    return <div>
-        {state.isLoading && state.error == null && <h1>Authentification callback processing...</h1>}
-        {!state.isLoading && state.error != null && <h1>Error after callback</h1>}
-
+    return <div className="login-callback-root">
+        {state.isLoading && state.error == null && <CircularProgress color="primary" />}
+        {!state.isLoading && state.error != null && <div className="login-callback-error">
+            <Typography variant="h6">Sorry about that!</Typography>
+        <Typography variant="subtitle1">An error occured while we were trying to log you in. (Error message: {state.error.message})</Typography>
+        </div>}
     </div>;
 }
 
