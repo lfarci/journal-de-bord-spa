c58ded4eee63fff9155f0e0dc07d9e3e6ec0e711
Logan Farci <farci.logan@gmail.com>
2020-10-15 23:34:15 +0200

    issue/#15: props added to the ProfileHeader component

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -12,20 +12,20 @@ import ProfileSection from "./ProfileSection";
 import ProfileHeader from "./ProfileHeader";
 
 function Profile() {
-    return <Page title="Profile" showBackButton>
-        <ProfileHeader />
-        <ProfileSection title="General" divider>
-            <ProfileProperty label="Email" value="farci.logan@gmail.com" />
-            <ProfileProperty label="Distance objective" value="1500 kilometers" renderIcon={() => <EditRoundedIcon style={{ color: "c4c4c4" }}/>} />
-        </ProfileSection>
-        <ProfileSection title="Data" divider>
-            <ProfileProperty label="Export my journal" renderIcon={() => <GetAppRoundedIcon style={{ color: "c4c4c4" }} />} />
-            <ProfileProperty label="Delete my journal" renderIcon={() => <DeleteRoundedIcon style={{ color: "c4c4c4" }} />} />
-        </ProfileSection>
-        <ProfileSection>
-            <ProfileProperty label="Log out" />
-        </ProfileSection>
-    </Page>
+	return <Page title="Profile" showBackButton>
+		<ProfileHeader name="Logan Farci" picture="https://scontent.fbru2-1.fna.fbcdn.net/v/t31.0-8/18209356_1613491082024964_6359923658550097249_o.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=HBdEpwICukIAX8u77kv&_nc_ht=scontent.fbru2-1.fna&oh=9c556f735209987b473a1aa44f1226a1&oe=5FAF9C2F" />
+		<ProfileSection title="General" divider>
+			<ProfileProperty label="Email" value="farci.logan@gmail.com" />
+			<ProfileProperty label="Distance objective" value="1500 kilometers" renderIcon={() => <EditRoundedIcon style={{ color: "c4c4c4" }}/>} />
+		</ProfileSection>
+		<ProfileSection title="Data" divider>
+			<ProfileProperty label="Export my journal" renderIcon={() => <GetAppRoundedIcon style={{ color: "c4c4c4" }} />} />
+			<ProfileProperty label="Delete my journal" renderIcon={() => <DeleteRoundedIcon style={{ color: "c4c4c4" }} />} />
+		</ProfileSection>
+		<ProfileSection>
+			<ProfileProperty label="Log out" />
+		</ProfileSection>
+	</Page>
 }
 
 export default Profile;
--- src/components/pages/profile/ProfileHeader.tsx
+++ src/components/pages/profile/ProfileHeader.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-function ProfileHeader() {
-    return <div className="profile-header-root">
-        <img className="profile-header-root-picture" src="https://scontent.fbru2-1.fna.fbcdn.net/v/t31.0-8/18209356_1613491082024964_6359923658550097249_o.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=HBdEpwICukIAX8u77kv&_nc_ht=scontent.fbru2-1.fna&oh=9c556f735209987b473a1aa44f1226a1&oe=5FAF9C2F" />
-        <p className="profile-header-title">Logan Farci</p>
-    </div>;
+interface IProfileHeaderProps {
+	/**
+	 * The URI to the user picture.
+	 */
+	picture: string;
+	/**
+	 * The name should be in a "GivenName FamilyName" format.
+	 */
+	name: string;
+}
+
+function ProfileHeader(props: IProfileHeaderProps) {
+	return <div className="profile-header-root">
+		<img className="profile-header-root-picture" src={props.picture} alt={props.name} />
+		<p className="profile-header-title">{props.name}</p>
+	</div>;
 }
 
 export default ProfileHeader;
