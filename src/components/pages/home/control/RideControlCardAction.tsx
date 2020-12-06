dbcf875af86660d0b13dd023117e093a64372def
Logan Farci <farci.logan@gmail.com>
2020-12-06 14:55:57 +0100

    enhancement/#23: the RideControlCardAction component is tested and has a default text value

--- src/components/pages/home/control/RideControlCardAction.tsx
+++ src/components/pages/home/control/RideControlCardAction.tsx
@@ -8,13 +8,18 @@ export interface IRideControlCardActionProps {
 }
 
 export default function RideControlCardAction(props: IRideControlCardActionProps) {
+
+    const defaultText = "Click";
+
+    const getText = () => props.text === "" ? defaultText : props.text;
     const getColor = () => props.color === undefined ? "primary" : props.color;
     const getAction = () => props.onClick === undefined ? () => {} : props.onClick;
+
     return <Button size="medium"
         variant="contained"
         color={getColor()}
         onClick={getAction()}
     >
-        {props.text}
+        {getText()}
     </Button>;
 }
--- src/components/pages/home/control/__test__/RideControlCard.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import RideControlCard from "../RideControlCard";
-
-it('renderes as expected', () => {
-    const tree = renderer.create(<RideControlCard />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
