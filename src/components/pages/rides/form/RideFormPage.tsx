5553544329280403318a4dee8e5ed6cdccd9b2dd
Logan Farci <farci.logan@gmail.com>
2021-01-04 14:40:07 +0100

    bug/#16: all the indentations are transformed to space in the RideFormPage source code

--- src/components/pages/rides/form/RideFormPage.tsx
+++ src/components/pages/rides/form/RideFormPage.tsx
@@ -5,29 +5,29 @@ import { TrafficCondition } from "./fields";
 import RideForm from "./RideForm";
 
 const defaultRide: Ride = {
-	departure: {
-		moment: new Date(2021, 1, 1, 12, 0, 0),
-		location: {
-			id: 3,
-			name: "Magasin",
-			latitude: 23.45,
-			longitude: 23.45
-		},
-		odometerValue: 10000
-	},
-	arrival: {
-		moment: new Date(2021, 1, 1, 13, 0, 0),
-		location: {
-			id: 4,
-			name: "Maison",
-			latitude: 25,
-			longitude: 26
-		},
-		odometerValue: 12000
-	},
-	driverPseudonym: undefined,
-	trafficCondition: TrafficCondition.NORMAL,
-	comment: "Je suis un brave."
+    departure: {
+        moment: new Date(2021, 1, 1, 12, 0, 0),
+        location: {
+            id: 3,
+            name: "Magasin",
+            latitude: 23.45,
+            longitude: 23.45
+        },
+        odometerValue: 10000
+    },
+    arrival: {
+        moment: new Date(2021, 1, 1, 13, 0, 0),
+        location: {
+            id: 4,
+            name: "Maison",
+            latitude: 25,
+            longitude: 26
+        },
+        odometerValue: 12000
+    },
+    driverPseudonym: undefined,
+    trafficCondition: TrafficCondition.NORMAL,
+    comment: "Je suis un brave."
 };
 
 interface IRideFormPageState {
@@ -44,13 +44,13 @@ function RideFormPage() {
 
     const setLoading = (value: boolean) => setState((prev) => ({...prev, isLoading: value }));
 
-	return <RideForm
-		ride={defaultRide}
+    return <RideForm
+        ride={defaultRide}
         isDriving={false}
         isLoading={state.isLoading}
         error={state.error}
-		onChange={() => { }}
-		onSubmit={async (data: Ride) => {
+        onChange={() => { }}
+        onSubmit={async (data: Ride) => {
             const resources = new ResourcesService();
             try {
                 setLoading(true);
@@ -60,7 +60,7 @@ function RideFormPage() {
                 setState((prev) => ({...prev, isLoading: false, error: error }))
             }
         }}
-	/>;
+    />;
 }
 
 export default RideFormPage;
